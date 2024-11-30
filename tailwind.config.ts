5088d6044cac20f5693504dc24935bb7a381e75c
naruko_hstk <projectcsj@gmail.com>
2024-11-30 19:40:37 +0800

    refactor: full reset project
    完整重設專案以追加shadcn/ui框架

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,18 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      screens: {
-        desktop: "1920px",
-        tablet: "1024px",
-        mobile: "375px",
-      },
-      fontSize: {
-        h1: "48px",
-        h2: "32px",
-        h3: "24px",
-        normal: "16px",
-        "h1-mobile": "30px",
-        "h2-mobile": "24px",
-        "h3-mobile": "20px",
-        "normal-mobile": "16px",
-      },
       colors: {
-        primary: "",
-        secondary: "",
-        complete: "",
-        todo: "",
-        "not-support": "",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
